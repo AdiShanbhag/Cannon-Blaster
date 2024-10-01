fcb4f34451e81b8c4f8f9be6e2b2f65ff39fa6ea
Aditya <aditya.d.shanbhag25@gmail.com>
2024-10-01 14:55:08 +0530

    1st Screen complete

--- Assets/Scripts/PlayButtonController.cs
+++ Assets/Scripts/PlayButtonController.cs
@@ -5,38 +5,78 @@ using UnityEngine.UI;
 
 public class PlayButtonController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public GameObject popUpMenu;
+    public Button[] buttons;
+    public GameObject[] popUpMenus;
 
     private Vector3 originalSize;
-    private bool isHighlighted = true;
+    private Button currentSelectedButton;
 
-    private void Awake()
-    {
-        this.originalSize = transform.localScale;
-    }
+    private GameObject currentPopUpMenu;
 
     void Start()
     {
-        transform.localScale = this.originalSize * 1.5f;
-
-        this.popUpMenu.SetActive(true);
-    }
-
-    public void OnPlayButtonClick(Button button)
-    {
-        if(isHighlighted)
+        // Store the original size of the buttons
+        if(this.buttons.Length > 0)
         {
-            button.transform.localScale = this.originalSize * 1.5f;
-            //isHighlighted = true;
+            this.originalSize = this.buttons[0].transform.localScale;
         }
-        this.popUpMenu.SetActive(true);
+        // Set the Play button as the default selected and enlarged button
+        this.currentSelectedButton = this.buttons[2];
+        EnlargeButton(this.currentSelectedButton);
+
+        // Set the corresponding Play button pop-up menu as the default
+        this.currentPopUpMenu = this.popUpMenus[2];
+        this.currentPopUpMenu.SetActive(true);
+
+        // Disable other pop-up menus except for the Play button's by default
+        for(int i = 0; i < this.popUpMenus.Length; i++)
+        {
+            if(i != 2)
+            {
+                this.popUpMenus[i].SetActive(false);
+            }
+        }
     }
 
-    public void CloseMenu()
+    // Method to handle any button click
+    public void OnButtonClick(Button clickedButton)
     {
-        this.transform.localScale = this.originalSize;
-        this.isHighlighted = false;
-        this.popUpMenu.SetActive(false);
+        // Restore the size of the currently selected button
+        if(this.currentSelectedButton != null)
+        {
+            RestoreButtonSize(this.currentSelectedButton);
+        }
+
+        // Enlarge the clicked button and set it as the new selected button
+        EnlargeButton(clickedButton);
+        this.currentSelectedButton = clickedButton;
+
+        // Get the index of the clicked button in the array
+        int clickedButtonIndex = System.Array.IndexOf(buttons, clickedButton);
+
+        // Close the currently active pop-up menu and open the new one
+        if (this.currentPopUpMenu != null)
+        {
+            this.currentPopUpMenu.SetActive(false);
+        }
+
+        // Set the corresponding pop-up menu for the clicked button as active
+        if (clickedButtonIndex >= 0 && clickedButtonIndex < popUpMenus.Length)
+        {
+            this.currentPopUpMenu = popUpMenus[clickedButtonIndex];
+            this.currentPopUpMenu.SetActive(true);
+        }
+    }
+
+    // Method to enlarge a button by 20%
+    private void EnlargeButton(Button button)
+    {
+        button.transform.localScale = originalSize * 1.2f;
+    }
+
+    // Method to restore the button to its original size
+    private void RestoreButtonSize(Button button)
+    {
+        button.transform.localScale = originalSize;
     }
 }
