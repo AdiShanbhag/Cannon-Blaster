abf28c2b0b79d5051790c330361d46c07cfe00b3
Aditya <aditya.d.shanbhag25@gmail.com>
2024-10-03 11:23:13 +0530

    Transition error fixed
    Background and player transition together. Player movement script seperated from game manager
    Player is clamped to move within the screen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.PlayerLoop;
 
 public class GameManager : MonoBehaviour
@@ -12,162 +13,78 @@ public class GameManager : MonoBehaviour
 
     //Transitioning backgroundImage
     public RectTransform backgroundImage;
-    public float targetBottomValue = 0f;
     public float transitionDuration = 1f;
-    private float currentBottomValue;
     private float initialBottomValue = 300f;
-    private bool startTransition = false;
-    private float tranisitionProgress = 0f;
 
     //Transitioning Player
     public Transform player;
-    public float leftLimit = -3f;
-    public float rightLimit = 3f;
-    public float horizontalSpeed = 2f;
     public float verticalStartY = -5f;
     public float verticalTargetY = -6.5f;
-    public float playerControlSpeed = 5f;
-    private bool shouldPlayerMove = false;
-    private bool isTransitioning = false;
-    private bool isGameReady = false;
-    private bool isPlayerControlled = false;
-    private Vector3 direction = Vector3.right;
+    public bool isGameReady {get; private set;}
+
+    private bool isGamePaused = false;
 
 
     public void Start()
     {
-        /*this.currentBottomValue = this.initialBottomValue;
-        SetBottomValue(this.initialBottomValue); */
-
         this.player.position = new Vector3(player.position.x, this.verticalStartY, player.position.z);
-        this.shouldPlayerMove = true;
+        this.backgroundImage.offsetMin = new Vector2(this.backgroundImage.offsetMin.x, this.initialBottomValue);
 
         this.readyPopUpPanel.SetActive(false);
         this.pausePopUpPanel.SetActive(false);
-    }
 
-    void Update()
-    {
-        /* if(this.startTransition)
-        {
-            SmoothTransition();
-        }*/
-
-        if(this.shouldPlayerMove && !this.isPlayerControlled)
-        {
-            MovePlayerLeftRight();
-        }
-
-        if(this.isTransitioning)
-        {
-            SmoothBackgroundTransition();
-            SmoothPlayerVerticalTransition();
-        }
-
-        if(this.isGameReady && !this.isPlayerControlled)
-        {
-            DetectPlayerInput();
-        }
-
-        if(this.isPlayerControlled)
-        {
-            PlayerControlledMovement();
-        }
+        Time.timeScale = 1;
     }
 
     public void OnClickPlay()
     {
-        /* this.startTransition = true;
-        this.tranisitionProgress = 0f; */
-        this.isTransitioning = true;
-
         this.uiGameObject.SetActive(false);
         this.readyPopUpPanel.SetActive(true);
+
+        StartCoroutine(TransitionBackgroundAndPlayer());
     }
 
-    private void MovePlayerLeftRight()
+    private IEnumerator TransitionBackgroundAndPlayer()
     {
-        this.player.position += this.direction * this.horizontalSpeed * Time.deltaTime;
+        float elapsedTime = 0f;
 
-        if(this.player.position.x > this.rightLimit)
+        while(elapsedTime < this.transitionDuration)
         {
-            this.direction = Vector3.left;
+            float newBottom = Mathf.Lerp(this.initialBottomValue, 0f, elapsedTime / this.transitionDuration);
+            this.backgroundImage.offsetMin = new Vector2(this.backgroundImage.offsetMin.x, newBottom);
+
+            float currentY = Mathf.Lerp(this.verticalStartY, this.verticalTargetY, elapsedTime / this.transitionDuration);
+            this.player.position = new Vector3(this.player.position.x, currentY, this.player.position.z);
+
+            elapsedTime += Time.deltaTime;
+
+            yield return null;
         }
-        else if(this.player.position.x < this.leftLimit)
-        {
-            this.direction = Vector3.right;
-        }
-    }
-    private void SmoothBackgroundTransition()
-    {
-        this.tranisitionProgress += Time.deltaTime / this.transitionDuration;
-        this.currentBottomValue = Mathf.Lerp(this.initialBottomValue, this.targetBottomValue, this.tranisitionProgress);
-        SetBottomValue(this.currentBottomValue);
 
-        if(this.tranisitionProgress >= 1f)
-        {
-            this.startTransition = false;
-            this.isGameReady = true;
-        }
-        /*
-        float currentBottom = Mathf.Lerp(300f, 0f, Time.deltaTime * this.transitionDuration);
-        this.backgroundImage.offsetMin = new Vector2(this.backgroundImage.offsetMin.x, currentBottom);
+        this.backgroundImage.offsetMin = new Vector2(this.backgroundImage.offsetMin.x, 0f);
+        this.player.position = new Vector3(this.player.position.x, this.verticalTargetY, this.player.position.z);
 
-        if(Mathf.Abs(this.backgroundImage.offsetMin.y) < 0.01f)
-        {
-            this.isTransitioning = false;
-            this.isGameReady = true;
-        } */
-
-    }
-
-    private void SmoothPlayerVerticalTransition()
-    {
-        float currentY = Mathf.MoveTowards(this.player.position.y, this.verticalTargetY, this.transitionDuration * Time.deltaTime);
-        this.player.position = new Vector3(this.player.position.x, currentY, this.player.position.z);
-    }
-
-    private void DetectPlayerInput()
-    {
-        if(Input.GetMouseButtonDown(0) || Input.touchCount > 0 || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
-        {
-            this.shouldPlayerMove = false;
-
-            this.isPlayerControlled = true;
-
-            this.readyPopUpPanel.SetActive(false);
-        }
-    }
-
-    private void PlayerControlledMovement()
-    {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        Vector3 playerMovement = new Vector3(moveHorizontal, 0f, 0f) * this.playerControlSpeed * Time.deltaTime;
-
-        this.player.position += playerMovement;
-    }
-
-    /* public void OnReadyScreenInput()
-    {
-        if(this.isGameReady)
-        {
-            this.shouldPlayerMove = true;
-        }
-    } */
-
-    private void SetBottomValue(float bottomValue)
-    {
-        this.backgroundImage.offsetMin = new Vector2(this.backgroundImage.offsetMin.x, bottomValue);
-        
+        this.isGameReady = true;
     }
 
     public void OnClickPause()
     {
-        this.pausePopUpPanel.SetActive(true);
+        if(!this.isGamePaused)
+        {
+            Time.timeScale = 0;
+            this.pausePopUpPanel.SetActive(true);
+            this.isGamePaused = true;
+        }
+
     }
 
     public void OnClickResume()
     {
-        this.pausePopUpPanel.SetActive(false);
+        if(this.isGamePaused)
+        {
+            Time.timeScale = 1;
+            this.pausePopUpPanel.SetActive(false);
+            this.isGamePaused = false;
+        }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,77 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public float leftLimit = -3f;
+    public float rightLimit = 3f;
+    public float horizontalSpeed = 2f;
+    public float playerControlSpeed = 5f;
+    private bool isPlayerControlled = false;
+    private Vector3 direction = Vector3.right;
+    public GameObject readyPopUpPanel;
+    
+    private GameManager gameManager;
     void Start()
     {
-        
+        this.isPlayerControlled = false;
+
+        this.gameManager = FindObjectOfType<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(!this.isPlayerControlled)
+        {
+            MovePlayerLeftRight();
+        }
+
+        if(this.gameManager != null && this.gameManager.isGameReady)
+        {
+            PlayerControlledMovement();
+            DetectPlayerInput();
+        }
+    }
+
+    private void MovePlayerLeftRight()
+    {
+        this.transform.position += this.direction * this.horizontalSpeed * Time.deltaTime;
+
+        if(this.transform.position.x > this.rightLimit)
+        {
+            this.direction = Vector3.left;
+        }
+        else if(this.transform.position.x < this.leftLimit)
+        {
+            this.direction = Vector3.right;
+        }
+    }
+
+    private void DetectPlayerInput()
+    {
+        if(Input.GetMouseButtonDown(0) || Input.touchCount > 0 || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+        {
+            this.isPlayerControlled = true;
+            this.readyPopUpPanel.SetActive(false);
+        }
+    }
+
+    private void PlayerControlledMovement()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector3 playerMovement = new Vector3(moveHorizontal, 0f, 0f) * this.playerControlSpeed * Time.deltaTime;
+
+        this.transform.position += playerMovement;
+
+        RestrictMovementWithinScreen();
+    }
+
+    private void RestrictMovementWithinScreen()
+    {
+        Vector3 playerViewportPos = Camera.main.WorldToViewportPoint(this.transform.position);
+        playerViewportPos.x = Mathf.Clamp(playerViewportPos.x, 0f, 1f);
+
+        Vector3 clampedWorldPos = Camera.main.ViewportToWorldPoint(playerViewportPos);
+        this.transform.position = new Vector3(clampedWorldPos.x, this.transform.position.y, this.transform.position.z);
     }
 }
